22d34aea88a2f854a4c2f5d5f74fc945e87bf365
sankari123 <write2sankari@yahoo.com>
2017-09-19 18:46:12 +0530

    Add files via upload

---
+++ readCSV.php
@@ -0,0 +1,20 @@
+ 
+<?php
+$csv_output='';
+ 
+//$csv_output=" <table>\n\n";
+//$f = fopen("xport.csv", "r");
+//while (($line = fgetcsv($f)) !== false) {
+ //       $csv_output.= "<tr>";
+  //      foreach ($line as $cell) {
+      //          $csv_output.= "<td>" . htmlspecialchars($cell) . "</td>";
+  //      }
+  //      $csv_output.= "</tr>\n";
+//}
+//fclose($f);
+//$csv_output.= "\n</table> ";
+$csv_output= "sdfjsdkfjsdlfjsdlfsdjlfsd";
+
+echo $csv_output;
+?>
+ 
